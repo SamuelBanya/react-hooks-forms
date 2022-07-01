023170c9f21c6beabdf9810aa909d0e04c06997c
Samuel Banya <sbanya@fastmail.com>
2022-06-30 21:30:00 -0400

    Adding latest changes to assignment

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,33 @@
+// This is a refactor so that we can place the state component into
+// the parent component
+
+import React, { useState } from "react";
+import Form from "./Form";
+import DisplayData from "./DisplayData";
+
+function ParentComponent() {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
+  return (
+    <div>
+      <Form
+        firstName={firstName}
+        lastName={lastName}
+        handleFirstNameChange={handleFirstNameChange}
+        handleLastNameChange={handleLastNameChange}
+      />
+      <DisplayData firstName={firstName} lastName={lastName} />
+    </div>
+  );
+}
+
+export default ParentComponent;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React from "react";
+import ReactDOM from "react-dom";
+import ParentComponent from "./components/ParentComponent";
+import NewsletterForm from "./components/NewsletterForm";
+import NumberForm from "./components/NumberForm";
 
+// NOTE: I revised this to include the shorthand for '<ReactComponent>
+// so that I can utilize the 'Form' AND 'NewsletterForm'
+// mostly because the lab itself doesn't even utilize the newsletter
+// form which I find weird --> If you build it, just use it too!
+
+// OTHER NOTE:
+// We also utilize the 'ParentComponent' which uses the 'Form'
+// child component accordingly in this scenario:
 ReactDOM.render(
-  <Form />,
-  document.getElementById('root')
+  <>
+    <ParentComponent />
+    <NewsletterForm />
+    <NumberForm />
+  </>,
+  document.getElementById("root")
 );
